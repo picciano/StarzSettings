90e5e948f157cfac2cbf9244e38e01bcdb97f409
Anthony Picciano <picciano1@anthonys-macbook-pro.local>
2015-11-12 08:59:02 -0700

    Initial Commit

---
+++ StarzSettings/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StarzSettings
+//
+//  Created by Anthony Picciano on 11/12/15.
+//  Copyright © 2015 Anthony Picciano. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
