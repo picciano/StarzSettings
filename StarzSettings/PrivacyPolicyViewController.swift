cbc79c7449f338145befadce86cc55414b56844f
Anthony Picciano <picciano1@anthonys-macbook-pro.local>
2015-11-13 10:46:18 -0700

    Add TOS and PP views

--- StarzSettings/PrivacyPolicyViewController.swift
+++ StarzSettings/PrivacyPolicyViewController.swift
@@ -7,29 +7,19 @@
 //
 
 import UIKit
+import StarzLegal
 
 class PrivacyPolicyViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        PrivacyPolicy.sharedInstance.loadPrivacyPolicyText { (result, error) -> Void in
+            if let result = result {
+                self.textView.text = result
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- StarzSettings/TermsOfServiceViewController.swift
+++ StarzSettings/TermsOfServiceViewController.swift
@@ -8,28 +8,20 @@
 
 import UIKit
 
+import StarzLegal
+
 class TermsOfServiceViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        TermsOfService.sharedInstance.loadTermsOfServiceText { (result, error) -> Void in
+            if let result = result {
+                self.textView.text = result
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
